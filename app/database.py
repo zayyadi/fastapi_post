101d1ee5e4ab99f0d0d3478f33658dd3d7760b4d
Zayyad Musa <alias.co>
2022-05-19 15:27:54 +0100

    done

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@ import os
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.environ.get("DB_URI")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
